e1d0a4245431550c01c2cf544f98781460f82b52
Yushi23 <ayushiev2014@mail.ru>
2024-02-22 17:55:31 +0800

    Add new validator

--- src/Validator.php
+++ src/Validator.php
@@ -2,21 +2,40 @@
 
 namespace Hexlet\Validator;
 
-require_once 'schemes/StringScheme.php';
-require_once 'schemes/NumberScheme.php';
-
 use Hexlet\Validator\Schemes\StringScheme;
 use Hexlet\Validator\Schemes\NumberScheme;
+use Hexlet\Validator\Schemes\ArrayScheme;
 
 class Validator
 {
-    public function string(): object
+    private $customValidatorFns;
+
+    public function string(): StringScheme
     {
-        return new StringScheme();
+        return new StringScheme($this);
     }
 
-    public function number(): object
+    public function number(): NumberScheme
     {
-        return new NumberScheme();
+        return new NumberScheme($this);
+    }
+
+    public function array(): ArrayScheme
+    {
+        return new ArrayScheme($this);
+    }
+
+    public function addValidator(string $type, string $name, callable $fn): void
+    {
+        if ($type == 'string' || $type == 'array' || $type == 'number') {
+            $this->customValidatorFns[$type][$name] = $fn;
+        } else {
+            throw new \Exception('boom!');
+        }
+    }
+
+    public function getValidator(): array
+    {
+        return $this->customValidatorFns;
     }
 }
